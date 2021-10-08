6f3bd381cf50ecefb969b8de463800fe33c9833a
phokhanhhung <phokhanhhung2409@gmail.com>
2021-10-08 20:49:20 +0700

    add src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Count from './Components/Count';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Count name="Count-1" onClick={() => {alert('hvefuy we')}}>This is Count 1</Count>
     </div>
   );
 }
