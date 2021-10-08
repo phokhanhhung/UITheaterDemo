2dfe63d784bb3a079c1f519b5a24409dd6a79b15
phokhanhhung <phokhanhhung2409@gmail.com>
2021-10-08 20:58:16 +0700

    add all

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Count from './Components/Count';
 function App() {
   return (
     <div className="App">
-      <Count name="Count-1" onClick={() => {alert('hvefuy we')}}>This is Count 1</Count>
     </div>
   );
 }
