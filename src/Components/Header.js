aa741f228b9d520399d712e26b04ecb84223bb57
TuanLeAnh1003 <19520331@gm.uit.edu.vn>
2021-10-11 10:46:41 +0700

    Tuan moi push

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../Images/logo.svg';
+import logo from '../Images/logo.png';
 import logoTicket from '../Images/tickets.svg';
 import './Header.css'
 import { Link } from "react-router-dom";
